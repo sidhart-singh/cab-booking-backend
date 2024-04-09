701182fce2cad8a8181e57886fab4b20b19bccc6
Sidhart Singh <isidhartsingh@gmail.com>
2024-04-09 15:09:41 +0530

    Update

--- src/main/java/com/sidhartsingh/config/AppConfig.java
+++ src/main/java/com/sidhartsingh/config/AppConfig.java
@@ -2,16 +2,14 @@ package com.sidhartsingh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 @Configuration
@@ -22,11 +20,11 @@ public class AppConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             return http
                     .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/api/auth/**").authenticated()
-                            .anyRequest().permitAll())
+                            .requestMatchers("/home", "/api/auth/**").permitAll()
+                            .anyRequest().authenticated())
                     .csrf(csrf->csrf.disable())
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .addFilterBefore(new JwtTokenValidationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new JwtTokenValidationFilter(), BasicAuthenticationFilter.class)
                     .formLogin(Customizer.withDefaults())
                     .build();
     }
